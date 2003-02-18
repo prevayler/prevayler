5c3c6210802b6febdbc66445eb3a5eeb467a5777
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2003-02-18 07:08:26 +0000

    Clock support.

---
+++ src/org/prevayler/util/clock/BrokenClock.java
@@ -0,0 +1,28 @@
+// Prevayler(TM) - The Open-Source Prevalence Layer.
+// Copyright (C) 2001 Klaus Wuestefeld.
+// This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 2.1 as published by the Free Software Foundation. This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+package org.prevayler.util.clock;
+
+import java.util.Date;
+
+
+/** A deterministic Clock that always returns the same time until it is forced to advance. A new BrokenClock's time() starts off at new Date(Long.MIN_VALUE);
+ */
+class BrokenClock implements Clock, java.io.Serializable {
+
+	protected long _millis = Long.MIN_VALUE;
+	private Date _time = new Date(_millis);
+
+
+	public Date time() { return _time; }
+
+
+	synchronized void advanceTo(Date newTime) {
+		long newMillis = newTime.getTime();
+		if (newMillis <= _millis) throw new RuntimeException("A Clock can only be set to the future. Current time: " + _time + " new time: " + newTime);
+		_millis = newMillis;
+		_time = newTime;
+	}
+
+}
---
+++ src/org/prevayler/util/clock/Clock.java
@@ -0,0 +1,17 @@
+// Prevayler(TM) - The Open-Source Prevalence Layer.
+// Copyright (C) 2001 Klaus Wuestefeld.
+// This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 2.1 as published by the Free Software Foundation. This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+package org.prevayler.util.clock;
+
+
+/** The clock used by every business object in a prevalent system for ALL its date/time related functions.
+ */
+public interface Clock {
+
+	/** Tells the time. The same value will be returned for every call, throughout the execution of a transaction, ensuring that each transaction is executed in a single moment in time. Without this, the transactions could not be deterministically re-executed during crash-recovery.
+	 * @return A Date greater or equal to the one returned by the last call to this method. If the time is the same as the last call, the SAME Date object is returned rather than a new, equal one.
+	 */
+	public java.util.Date time();
+
+}
