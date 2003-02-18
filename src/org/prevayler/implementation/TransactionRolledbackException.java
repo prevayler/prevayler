585a23fe846df3362939b3cf1b58a8db9c89515d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2003-02-18 07:05:40 +0000

    *** empty log message ***

--- src/org/prevayler/implementation/TransactionRolledbackException.java
+++
@@ -1,9 +0,0 @@
-package org.prevayler.implementation;
-
-import java.io.IOException;
-
-public class TransactionRolledbackException extends IOException {
-    public TransactionRolledbackException(Exception e) {
-        initCause(e);
-    }
-}
