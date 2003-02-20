130a473fa865bf49c5663b937616fa513765f208
Jon Tirsen <jon@tirsen.com>
2003-02-20 21:29:56 +0000

    Restructured src, splitted test-code and main-code.

---
+++ src/test/org/prevayler/test/AllTestSuite.java
@@ -0,0 +1,9 @@
+package org.prevayler.test;
+
+import junit.framework.TestCase;
+
+public class AllTestSuite extends TestCase {
+    public void test() throws Exception {
+        Main.main(new String[0]);
+    }
+}
