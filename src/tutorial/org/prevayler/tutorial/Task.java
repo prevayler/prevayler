c14ee5a4ad0dcf23f9b06de392c39f549cd5eec6
Carlos Villela <carlos@bluebox.com.br>
2004-03-08 22:05:18 +0000

    *** empty log message ***

--- src/tutorial/org/prevayler/tutorial/Task.java
+++ src/tutorial/org/prevayler/tutorial/Task.java
@@ -8,80 +8,59 @@ package org.prevayler.tutorial;
 
 import java.io.Serializable;
 
-/**
- * A simple task to be performed.
- * 
- * @author Carlos Villela
- * @since Mar 7, 2004
- */
+// START SNIPPET: task
 public class Task implements Serializable {
 
+    // END SNIPPET: task
+
     public static final int MAX_PRIORITY = 10;
+
     public static final int MED_PRIORITY = 5;
+
     public static final int MIN_PRIORITY = 0;
 
+    // START SNIPPET: fields
     private String description;
+
     private int priority;
 
-    /**
-     * Creates a new Task.
-     */
+    // END SNIPPET: fields
+
+    // START SNIPPET: ctors
     public Task() {
     }
 
-    /**
-     * Creates a new Task.
-     * 
-     * @param description
-     *            description of this task.
-     * @param priority
-     *            priority of this task.
-     */
     public Task(String description, int priority) {
         this.description = description;
         this.priority = priority;
     }
 
-    /**
-     * @return Returns the description.
-     */
+    //  END SNIPPET: ctors
+
+    // START SNIPPET: equals
+    public boolean equals(Object obj) {
+        if (obj instanceof Task) {
+            Task t = (Task) obj;
+            return (String.valueOf(description).equals(String.valueOf(t.description))) && (priority == t.priority);
+        }
+        return false;
+    }
+
+    // END SNIPPET: equals
+
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description
-     *            The description to set.
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return Returns the priority.
-     */
     public int getPriority() {
         return priority;
     }
 
-    /**
-     * @param priority
-     *            The priority to set.
-     */
     public void setPriority(int priority) {
         this.priority = priority;
-    }
-
-    /**
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    public boolean equals(Object obj) {
-        if (obj instanceof Task) {
-            Task t = (Task) obj;
-            return (String.valueOf(description).equals(String
-                    .valueOf(t.description)))
-                    && (priority == t.priority);
-        }
-        return false;
     }
 }
