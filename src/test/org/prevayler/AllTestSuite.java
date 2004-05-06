2eff04ed528356b0f8ff08ae54467c11af125f54
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2004-05-06 08:35:49 +0000

    Testing CVS

--- src/test/org/prevayler/AllTestSuite.java
+++ src/test/org/prevayler/AllTestSuite.java
@@ -2,6 +2,8 @@
 //Copyright (C) 2001-2003 Klaus Wuestefeld
 //This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+//Testing CVS
+
 package org.prevayler;
 
 import junit.framework.TestCase;
