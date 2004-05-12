6b9582eaa9b7e65ca7a8e22f82f12ccfc1a3054e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2004-05-12 07:27:10 +0000

    New DurableOutputStream by Justin Sampson. Simpler and safer.

--- src/main/org/prevayler/foundation/DurableOutputStream.java
+++ src/main/org/prevayler/foundation/DurableOutputStream.java
@@ -1,159 +1,4 @@
-//Prevayler(TM) - The Free-Software Prevalence Layer.
-//Copyright (C) 2001-2003 Klaus Wuestefeld
-//This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
+//Prevayler(TM) - The Free-Software Prevalence Layer.//Copyright (C) 2001-2004 Klaus Wuestefeld//This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.//Contributions: Justin Sampson.
 package org.prevayler.foundation;
-
-import java.io.*;
-
-
-public class DurableOutputStream {
-	private static final int NOT_CLOSED = 0;
-	private static final int CLOSE_CALLED = 1;
-	private static final int REALLY_CLOSED = 2;
-
-	private final File _file;
-	private final ObjectOutputStream _objectOutputStream;
-	private final FileOutputStream _fileOutputStream;
-	private final FileDescriptor _fileDescriptor;
-
-	private IOException _exceptionWhileSynching;
-	private IOException _exceptionWhileClosing;
-
-	private int _objectsWritten = 0;
-	private int _objectsSynced = 0;
-	private int _fileSyncCount = 0;
-
-	private int _closingState = NOT_CLOSED;
-
-	public DurableOutputStream(File file) throws IOException {
-		_file = file;
-		_fileOutputStream = new FileOutputStream(file);
-		_fileDescriptor = _fileOutputStream.getFD();
-		_objectOutputStream = new ObjectOutputStream(new BufferedOutputStream(_fileOutputStream, 16 * 512)); //Arbitrarily large buffer. Should be a power of two multiplied by 512 bytes (disk sector size).
-
-		startSyncher();
-	}
-
-	private void startSyncher() {
-		Thread syncher = new Thread() {
-			public void run() {
-				syncher();
-			}
-		};
-		syncher.setDaemon(true);
-		syncher.start();
-	}
-
-
-	public void sync(Object object, Turn myTurn) throws IOException {
-		int thisWrite;
-
-		try {
-			myTurn.start();
-			thisWrite = writeObject(object);
-		} finally {
-			myTurn.end();
-		}
-
-		waitUntilSynced(thisWrite);
-	}
-
-	private synchronized int writeObject(Object object) throws IOException {
-		if (_closingState != NOT_CLOSED) {
-			throw new IOException("stream is closing");
-		}
-
-		_objectOutputStream.writeObject(object);
-		_objectsWritten++;
-		notifyAll();
-		return _objectsWritten;
-	}
-
-	private synchronized void waitUntilSynced(int thisWrite) throws IOException {
-		while (_objectsSynced < thisWrite && _exceptionWhileSynching == null) {
-			Cool.wait(this);
-		}
-		if (_objectsSynced < thisWrite) {
-			throw _exceptionWhileSynching;
-		}
-	}
-
-	public synchronized void close() throws IOException {
-		if (_closingState == NOT_CLOSED) {
-			_closingState = CLOSE_CALLED;
-			notifyAll();
-		}
-
-		while (_closingState != REALLY_CLOSED) {
-			Cool.wait(this);
-		}
-
-		if (_exceptionWhileClosing != null) {
-			throw _exceptionWhileClosing;
-		}
-	}
-
-	private synchronized void syncher() {
-		try {
-			while (true) {
-				while (_objectsSynced == _objectsWritten && _closingState == NOT_CLOSED) {
-					Cool.wait(this);
-				}
-
-				if (_objectsSynced == _objectsWritten) {
-					break;
-				}
-
-				_objectOutputStream.flush();
-
-				_fileOutputStream.flush();
-				// ObjectOutputStream > BufferedOutputStream > FileOutputStream.
-				// The ObjectOutputStream will flush its underlying stream,
-				// but the BufferedOutputStream API documentation (JDK1.4.1_01)
-				// does not specify that it must flush its underlying stream too. :P
-				// The above line is here in case some obscure implementation doesn't
-				// flush the underlying stream.
-
-				Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-				// The above line should not be necessary but, with J2RE1.4.0_01 for Windows
-				// it actually produces 5x better results (!) for the
-				// org.prevayler.demos.scalability transaction execution test.
-				// NOT TESTED SINCE REFACTORING
-
-				_fileDescriptor.sync();
-
-				Thread.currentThread().setPriority(Thread.NORM_PRIORITY);
-
-				_objectsSynced = _objectsWritten;
-				_fileSyncCount++;
-
-				notifyAll();
-			}
-		} catch (IOException duringSync) {
-			_exceptionWhileSynching = duringSync;
-		} finally {
-			try {
-				_objectOutputStream.close();
-			} catch (IOException duringClose) {
-				_exceptionWhileClosing = duringClose;
-			}
-			_closingState = REALLY_CLOSED;
-			notifyAll();
-		}
-	}
-
-
-	public File file() {
-		return _file;
-	}
-
-	public synchronized int fileSyncCount() {
-		return _fileSyncCount;
-	}
-
-	public synchronized boolean reallyClosed() {
-		return _closingState == REALLY_CLOSED;
-	}
-
-}
+import java.io.*;
+public class DurableOutputStream {	/**	 * These two locks allow the two main activities of this class,	 * serializing transactions to a buffer on the one hand and flushing	 * the buffer and syncing to disk on the other hand, to proceed	 * concurrently. Note that where both locks are required, we always	 * acquire the _syncLock before acquiring the _writeLock to avoid	 * deadlock.	 */	private final Object _writeLock = new Object();	private final Object _syncLock = new Object();	/** The File object is only stashed for the sake of the file() getter. */	private final File _file;	/** All access guarded by _writeLock. */	private final ObjectOutputStream _objectOutputStream;	/** All access guarded by _syncLock. */	private final FileOutputStream _fileOutputStream;	/** All access guarded by _syncLock. */	private final FileDescriptor _fileDescriptor;	/** All access guarded by _writeLock. */	private ByteArrayOutputStream _active = new ByteArrayOutputStream();	/** All access guarded by _syncLock. */	private ByteArrayOutputStream _inactive = new ByteArrayOutputStream();	/** All access guarded by _writeLock. */	private boolean _closed = false;	/** All access guarded by _writeLock. */	private int _objectsWritten = 0;	/** All access guarded by _syncLock. */	private int _objectsSynced = 0;	/** All access guarded by _syncLock. */	private int _fileSyncCount = 0;	public DurableOutputStream(File file) throws IOException {		_file = file;		_fileOutputStream = new FileOutputStream(file);		_fileDescriptor = _fileOutputStream.getFD();		// All writes by _objectOutputStream go straight through to the _active		// buffer. Note that we can't just pass _active into the constructor		// because it gets swapped with _inactive, and we must always be		// writing to the current _active buffer. Accessing _active here is		// safe because we only write to or flush _objectOutputStream when we		// are already holding the _writeLock.		_objectOutputStream = new ObjectOutputStream(new OutputStream() {			public void write(byte[] b) throws IOException {				_active.write(b);			}			public void write(byte[] b, int off, int len) throws IOException {				_active.write(b, off, len);			}			public void write(int b) throws IOException {				_active.write(b);			}		});	}	public void sync(Object object, Turn myTurn) throws IOException {		int thisWrite;		// When a thread arrives here, all we care about at first is that it		// gets properly sequenced according to its turn.		try {			myTurn.start();			thisWrite = writeObject(object);		} finally {			myTurn.end();		}		// Now, having ended the turn, the next thread is allowed to come in		// and try to write its object before we get to the sync.		waitUntilSynced(thisWrite);	}	private int writeObject(Object object) throws IOException {		synchronized (_writeLock) {			if (_closed) {				throw new IOException("already closed");			}			try {				// Resetting an ObjectOutputStream allows it to forget all of				// the objects it has seen, which could improve memory usage				// during long periods of uptime. Otherwise, the				// ObjectOutputStream would have to remember the identity of				// every single object it had ever written out, just in case it				// were to be written again, so that it could write a				// back-reference instead of writing the object itself. Of				// course, that's not what we would want anyway, since				// transactions are supposed to be completely independent.				_objectOutputStream.writeObject(object);				_objectOutputStream.reset();			} catch (IOException exception) {				internalClose();				throw exception;			}			_objectsWritten++;			return _objectsWritten;		}	}	private void waitUntilSynced(int thisWrite) throws IOException {		// Here's the real magic. If this thread is the first to have written		// an object after a period of inactivity, and there are no other		// threads coming in, then thisWrite is trivially one greater than		// _objectsSynced, so this thread goes right ahead to sync its own		// object alone. But then, if another thread comes along and writes		// another object while this thread is syncing, it will write to the		// _active buffer and then promply block on the _syncLock until this		// thread finishes the sync. If threads continue to come in at just		// about the rate that syncs can happen, each thread will wait for the		// previous sync to complete and then initiate its own sync. The		// latency for the first thread is exactly the time for one sync, which		// is the minimum possible latency; the latency for any later thread is		// somewhere between that minimum and a maximum of two syncs, with the		// average being closer to the minimum end.		//		// Now, consider the steady state under heavy load. Some thread will		// always be syncing the _inactive buffer to disk, so every thread that		// arrives will write its object to the _active buffer and then wait		// here on the _syncLock. If 10 threads arrive during a given sync		// operation, then _active will hold 10 objects when that sync		// completes. As soon as that earlier thread releases _syncLock, one of		// those 10 new threads will acquire the lock and notice that its		// object has not yet been synced; it will then swap the buffers and		// flush and sync all 10 objects at once. Each of the 10 threads will		// acquire _syncLock in turn and now see that their object has already		// been synced and do nothing.		synchronized (_syncLock) {			if (_objectsSynced < thisWrite) {				int objectsWritten;				synchronized (_writeLock) {					if (_closed) {						throw new IOException("already closed");					}					try {						_objectOutputStream.flush();					} catch (IOException exception) {						internalClose();						throw exception;					}					ByteArrayOutputStream swap = _active;					_active = _inactive;					_inactive = swap;					objectsWritten = _objectsWritten;				}				try {					// Resetting the buffer clears its contents but keeps the					// allocated space. Therefore the buffers should quickly					// reach a steady state of an appropriate size and then not					// need to grow any more.					_inactive.writeTo(_fileOutputStream);					_inactive.reset();					_fileOutputStream.flush();					// Dropping the priority around the sync seems to have a					// somewhat favorable effect on throughput, at least on					// some Windows machines. Whether the effect has survived					// the various rewrites of this class enough to justify					// the continuing maintanence of this bit of code needs					// to be investigated.					Thread currentThread = Thread.currentThread();					int originalPriority = currentThread.getPriority();					currentThread.setPriority(Thread.MIN_PRIORITY);					try {						_fileDescriptor.sync();					} finally {						currentThread.setPriority(originalPriority);					}				} catch (IOException exception) {					internalClose();					throw exception;				}				_objectsSynced = objectsWritten;				_fileSyncCount++;			}		}	}	public void close() throws IOException {		synchronized (_syncLock) {			synchronized (_writeLock) {				if (_closed) {					return;				}				internalClose();				_fileOutputStream.close();			}		}	}	private void internalClose() {		synchronized (_writeLock) {			_closed = true;			_active = null;			_inactive = null;		}	}	public File file() {		return _file;	}	public synchronized int fileSyncCount() {		synchronized (_syncLock) {			return _fileSyncCount;		}	}	public boolean reallyClosed() {		synchronized (_writeLock) {			return _closed;		}	}}
--- src/test/org/prevayler/implementation/LogFileRollingTest.java
+++ src/test/org/prevayler/implementation/LogFileRollingTest.java
@@ -1,90 +1,1 @@
-//Prevayler(TM) - The Free-Software Prevalence Layer.
-//Copyright (C) 2001-2004 Klaus Wuestefeld
-//This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-package org.prevayler.implementation;
-
-import java.io.IOException;
-
-import org.prevayler.Prevayler;
-import org.prevayler.PrevaylerFactory;
-import org.prevayler.foundation.*;
-
-public class LogFileRollingTest extends FileIOTest {
-
-	private Prevayler _prevayler;
-	private String _prevalenceBase;
-	
-
-	public void testFileRolling() throws Exception {
-
-		crashRecover(""); //There is nothing to recover at first. A new system will be created.
-
-		append("a","a");
-		crashRecover("a");
-
-		append("b","ab"); //Starts new transactionLog (2).
-		append("c","abc");
-		append("d","abcd"); //Starts new transactionLog (4).
-		append("e","abcde");
-		append("f","abcdef");  //Starts new transactionLog (6).
-		crashRecover("abcdef");
-		
-		append("g","abcdefg"); //Starts new transactionLog (7).
-		snapshot();
-		append("h","abcdefgh");
-		append("i","abcdefghi");  //Starts new transactionLog (9).
-		append("j","abcdefghij");
-		crashRecover("abcdefghij");
-
-		_prevayler.close();
-		deleteFromTestDirectory("0000000000000000001.transactionLog");
-		deleteFromTestDirectory("0000000000000000002.transactionLog");
-		deleteFromTestDirectory("0000000000000000004.transactionLog");
-		deleteFromTestDirectory("0000000000000000006.transactionLog");
-		deleteFromTestDirectory("0000000000000000007.transactionLog");
-		deleteFromTestDirectory("0000000000000000007.snapshot");
-		deleteFromTestDirectory("0000000000000000009.transactionLog");
-		assertTrue(new java.io.File(_testDirectory).list().length == 0);
-	}
-
-	private void crashRecover(String expectedResult) throws Exception {
-		out("CrashRecovery.");
-		if (_prevayler != null) _prevayler.close();
-		PrevaylerFactory factory = new PrevaylerFactory();
-		factory.configureTransactionLogFileSizeThreshold(225);  //Enough to hold 2 transactions.
-		factory.configureTransactionLogFileAgeThreshold(0);  //Not being tested.
-		factory.configurePrevalenceBase(_testDirectory);
-		factory.configurePrevalentSystem(new AppendingSystem());
-		_prevayler = factory.create();
-		verify(expectedResult);
-	}
-
-	private void snapshot() throws IOException {
-		out("Snapshot.");
-		_prevayler.takeSnapshot();
-	}
-
-
-	private void append(String appendix, String expectedResult) throws Exception {
-		out("Appending " + appendix);
-		_prevayler.execute(new Appendix(appendix));
-		verify(expectedResult);
-	}
-
-
-	private void verify(String expectedResult) {
-		out("Expecting result: " + expectedResult);
-		assertEquals(expectedResult, system().value());
-	}
-
-
-	private AppendingSystem system() {
-		return (AppendingSystem)_prevayler.prevalentSystem();
-	}
-
-	private static void out(Object obj) {
-		if (false) System.out.println(obj);   //Change this line to see what the test is doing.
-	}
-
-}
+//Prevayler(TM) - The Free-Software Prevalence Layer.//Copyright (C) 2001-2004 Klaus Wuestefeld//This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.//Contributions: Justin Sampson.package org.prevayler.implementation;import java.io.IOException;import java.util.Collections;import java.util.Arrays;import org.prevayler.Prevayler;import org.prevayler.PrevaylerFactory;import org.prevayler.foundation.*;public class LogFileRollingTest extends FileIOTest {	private Prevayler _prevayler;	private String _prevalenceBase;		public void testFileRolling() throws Exception {		crashRecover(""); //There is nothing to recover at first. A new system will be created.		append("a","a");		crashRecover("a");		append("b","ab"); //Starts new transactionLog (2).		append("c","abc");		append("d","abcd"); //Starts new transactionLog (4).		append("e","abcde");		append("f","abcdef");  //Starts new transactionLog (6).		crashRecover("abcdef");				append("g","abcdefg"); //Starts new transactionLog (7).		snapshot();		append("h","abcdefgh");		append("i","abcdefghi");  //Starts new transactionLog (9).		append("j","abcdefghij");		crashRecover("abcdefghij");		_prevayler.close();		deleteFromTestDirectory("0000000000000000001.transactionLog");		deleteFromTestDirectory("0000000000000000002.transactionLog");		deleteFromTestDirectory("0000000000000000004.transactionLog");		deleteFromTestDirectory("0000000000000000006.transactionLog");		deleteFromTestDirectory("0000000000000000007.transactionLog");		deleteFromTestDirectory("0000000000000000007.snapshot");		deleteFromTestDirectory("0000000000000000009.transactionLog");		assertEquals(Collections.EMPTY_LIST,				Arrays.asList(new java.io.File(_testDirectory).list()));	}	private void crashRecover(String expectedResult) throws Exception {		out("CrashRecovery.");		if (_prevayler != null) _prevayler.close();		PrevaylerFactory factory = new PrevaylerFactory();		factory.configureTransactionLogFileSizeThreshold(226);  //Enough to hold 2 transactions.		factory.configureTransactionLogFileAgeThreshold(0);  //Not being tested.		factory.configurePrevalenceBase(_testDirectory);		factory.configurePrevalentSystem(new AppendingSystem());		_prevayler = factory.create();		verify(expectedResult);	}	private void snapshot() throws IOException {		out("Snapshot.");		_prevayler.takeSnapshot();	}	private void append(String appendix, String expectedResult) throws Exception {		out("Appending " + appendix);		_prevayler.execute(new Appendix(appendix));		verify(expectedResult);	}	private void verify(String expectedResult) {		out("Expecting result: " + expectedResult);		assertEquals(expectedResult, system().value());	}	private AppendingSystem system() {		return (AppendingSystem)_prevayler.prevalentSystem();	}	private static void out(Object obj) {		if (false) System.out.println(obj);   //Change this line to see what the test is doing.	}}
