d45d186f5fa19ec4d1ef59efaa4a8ca7f6ef6e4a
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2004-05-14 06:34:37 +0000

    Giving credit to Aleksey Aristov for ClassLoader configurability.

--- src/main/org/prevayler/PrevaylerFactory.java
+++ src/main/org/prevayler/PrevaylerFactory.java
@@ -1,6 +1,7 @@
 //Prevayler(TM) - The Free-Software Prevalence Layer.
 //Copyright (C) 2001-2003 Klaus Wuestefeld
 //This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//Contributions: Aleksey Aristov.
 
 package org.prevayler;
 
