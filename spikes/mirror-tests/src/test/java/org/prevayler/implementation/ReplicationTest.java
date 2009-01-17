c19b820a3bc85544632d5ecc0b85d9f03c3e04c8
Justin T. Sampson <justin@krasama.com>
2009-01-17 09:44:43 -0800

    Run ReplicationTest with real network, not mock.

--- spikes/mirror-tests/src/test/java/org/prevayler/implementation/ReplicationTest.java
+++ spikes/mirror-tests/src/test/java/org/prevayler/implementation/ReplicationTest.java
@@ -4,22 +4,17 @@
 
 package org.prevayler.implementation;
 
+import java.io.File;
+
 import org.prevayler.Prevayler;
 import org.prevayler.PrevaylerFactory;
 import org.prevayler.foundation.Cool;
 import org.prevayler.foundation.FileIOTest;
-import org.prevayler.foundation.network.NetworkMock;
-
-import java.io.File;
-import java.io.IOException;
 
 public class ReplicationTest extends FileIOTest {
 
-	private static final NetworkMock NETWORK_MOCK = new NetworkMock();
-
 	private Prevayler _server;
 	private Prevayler _client;
-	private Prevayler _clientWithServer;
 
 	public void testServerFirst() throws Exception {
 		serverCrashRecover(0);
@@ -110,18 +105,10 @@ public class ReplicationTest extends FileIOTest {
 		_client = factory.create();
 	}
 
-	private void clientWithServerCrashRecover(int remoteServerPortOffset, int serverPortOffset) throws IOException, ClassNotFoundException {
-		PrevaylerFactory factory = factory("clientWithServer");
-		factory.configureReplicationClient("localhost", PrevaylerFactory.DEFAULT_REPLICATION_PORT + remoteServerPortOffset);
-		factory.configureReplicationServer(PrevaylerFactory.DEFAULT_REPLICATION_PORT + serverPortOffset);
-		_clientWithServer = factory.create();
-	}
-
     private PrevaylerFactory factory(String directory) {
 		PrevaylerFactory factory = new PrevaylerFactory();
 		factory.configurePrevalentSystem(new AppendingSystem());
 		factory.configurePrevalenceDirectory(_testDirectory + File.separator + directory);
-		factory.configureNetwork(NETWORK_MOCK);
 		return factory;
 	}
 
@@ -129,7 +116,6 @@ public class ReplicationTest extends FileIOTest {
 	protected void tearDown() throws Exception {
 		_server = null;
 		_client = null;
-		_clientWithServer = null;
 		super.tearDown();
     }
 
@@ -138,7 +124,7 @@ public class ReplicationTest extends FileIOTest {
 	}
 
 	private String clientValue() {
-		Cool.sleep(100);  //The client is notified assynchronously.
+		Cool.sleep(100);  //The client is notified asynchronously.
 		return ((AppendingSystem)_client.prevalentSystem()).value();
 	}
 
