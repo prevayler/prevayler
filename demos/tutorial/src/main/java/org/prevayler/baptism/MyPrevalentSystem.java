2342f42df22342c3f2dffab71ab5a533854bbc19
Naveen Chawla <naveen.chwl@gmail.com>
2011-10-29 04:00:21 +0530

    Type-safe Prevayler

---
+++ demos/tutorial/src/main/java/org/prevayler/baptism/MyPrevalentSystem.java
@@ -0,0 +1,9 @@
+package org.prevayler.baptism;
+
+import java.util.Calendar;
+
+class MyPrevalentSystem implements java.io.Serializable{
+    public Calendar lookupCalendar(String id) {
+        return null;
+    }
+}
