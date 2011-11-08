a7a9320c1eeb9d92b9d6399db1b6796bfa8ca2d1
Naveen Chawla <naveen.chwl@gmail.com>
2011-11-08 10:46:38 +0530

    Type-safe Prevayler

--- tests/src/main/java/org/prevayler/implementation/NullQuery.java
+++ tests/src/main/java/org/prevayler/implementation/NullQuery.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 import org.prevayler.Query;
 
-public class NullQuery implements Query {
+public class NullQuery implements Query<Object,Object> {
 
 	public Object query(Object prevalentSystem, Date executionTime) throws Exception {
 		return null;
